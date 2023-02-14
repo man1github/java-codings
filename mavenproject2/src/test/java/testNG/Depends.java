757b12caaa64209a7db2d68b8da17ba61fe8ee1b
harichaser <harichaser@DESKTOP-L98MB0F>
2023-02-15 01:32:54 +0530

    second

---
+++ mavenproject2/src/test/java/testNG/Depends.java
@@ -0,0 +1,27 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class Depends {
+	
+  @Test(enabled = true)
+  public void foodbooking() {
+	  System.out.println("booking food");
+  }
+  @Test(dependsOnMethods = "foodbooking")
+  public void hotelConfirm() {
+	  System.out.println("hotel approval");
+  }
+  @Test(dependsOnMethods = "hotelConfirm")
+  public void deliverPartnerPickingOrder() {
+	  System.out.println("pick order from hotel by partner");
+  }
+  @Test(dependsOnMethods = "deliverPartnerPickingOrder")
+  public void collectingFood() {
+	  System.out.println("collecting Food from hotel");
+  }
+  @Test(dependsOnMethods = "collectingFood")
+  public void deliver() {
+	  System.out.println("food delivered");
+  }
+}
---
+++ mavenproject2/src/test/java/testNG/PriorityKeywordAndSkipTestCase.java
@@ -0,0 +1,40 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+
+public class PriorityKeywordAndSkipTestCase {
+	
+	@Test(priority=0)
+	public void lapopen() {
+		System.out.println("aaa");
+
+	}
+	@Test(priority=-1)
+	public void lapon() {
+		System.out.println("bbbb");
+
+	}
+	@Test(priority=-2)
+	public void refresh() {
+		System.out.println("ccc");
+	}
+	@Test(priority=1)
+	public void workStart() {
+		System.out.println("ddd");
+	}
+	@Test(priority=2,enabled = false)
+	public void workInbetweenBreak() {
+		System.out.println("ggg");
+	}
+	@Test(enabled = false)
+	public void workFinish() {
+		System.out.println("eee");
+	}
+	@Test
+	public void lapoff() {
+		System.out.println("fff");
+	}
+}
+
+
